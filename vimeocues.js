f423c40256a69c06f7e8d297aa52d18bffd0c5ae
Emily Lakic <elakic1@binghamton.edu>
2019-01-11 00:27:18 -0500

    Variables

--- vimeocues.js
+++ vimeocues.js
@@ -1,1 +1,17 @@
+/*Javascript program created by Emily Lakic, Jan. 2019*/
 
+//VARIABLES
+
+//cue
+var text;
+var cueTime;
+var id;
+var number = 0;
+
+//player
+var videoPlayer;
+var duration;
+
+//comparing cue and player time
+var currentcueTime;
+var time;
